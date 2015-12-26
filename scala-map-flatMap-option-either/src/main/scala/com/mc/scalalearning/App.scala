ac3ba494f0efc16d11a5425a88645be79312f605
Cliff <cburchfield@memberclicks.com>
2015-12-26 16:31:20 -0500

    Adding some map and flatMap examples

--- scala-map-flatMap-option-either/src/main/scala/com/mc/scalalearning/App.scala
+++ scala-map-flatMap-option-either/src/main/scala/com/mc/scalalearning/App.scala
@@ -5,11 +5,68 @@ package com.mc.scalalearning
  */
 object App {
   
-  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
-  
-  def main(args : Array[String]) {
-    println( "Hello World!" )
-    println("concat arguments = " + foo(args))
+  def procedurealToFunction()
+  {
+  	case class Foo(bar: Bar)
+  	case class Bar(baz: Baz)
+  	case class Baz(name: String)
+  	
+  	val item = Foo(Bar(Baz("Blah")))
+
+  	if(item != null)
+  	{
+  		val bar = item.bar
+  		if(bar != null)
+  		{
+  			val baz = bar.baz
+  			if(bar.baz != null)
+  			{
+  				return baz.name
+
+  			}
+  			else
+  			{
+  				return null
+  			}
+  		}
+  		else
+  		{
+  			return null
+  		} 
+  	}
+  	else
+  	{
+  		return null
+  	}
+
+
+  	//
+  	Option(item).flatMap{foo=>
+  		Option(foo.bar).flatMap{bar=>
+  			Option(bar.baz).map{baz=>
+  				baz.name
+  			}
+  		}
+  	}
+
+  	for{
+  		foo <- Option(item)
+  		bar <- Option(foo.bar)
+  		baz <- Option(bar.baz)
+  	} yield baz.name
   }
 
+  def mapFlatMap() {
+   		val list = "1" :: "2" :: "3" :: "4" :: Nil
+
+   		val result = list.map(i=>i.toInt)
+
+   		result.foreach(i=>println(i))
+
+   		val list2 = List(List("1", "2", "3"), List("4", "5", "6"))
+
+   		list2.flatMap(i=>i.map(j=>j.toInt))
+  }
 }
+
+
