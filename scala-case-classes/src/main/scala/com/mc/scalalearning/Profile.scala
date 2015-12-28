84bf0c7a51514272b9fc2f560217c720b401dddb
Cliff <cburchfield@memberclicks.com>
2015-12-28 07:04:28 -0500

    Reformatting

--- scala-case-classes/src/main/scala/com/mc/scalalearning/Profile.scala
+++ scala-case-classes/src/main/scala/com/mc/scalalearning/Profile.scala
@@ -1,7 +1,7 @@
 package com.mc.scalalearning
 
-case class Profile(name: Name, id: Long, groups: List[Group])
-
 case class Name(firstName: String, lastName: String)
 
 case class Group(id: Long, name: String)
+
+case class Profile(name: Name, id: Long, groups: List[Group])
